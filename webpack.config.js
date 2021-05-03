5b1d4cb41fd3680bab8ca764752a87f43d688a28
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-05-03 16:47:25 +0200

    bump version

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = ({ presets }, { mode }) =>
 			},
 			plugins: [
 				new webpack.DefinePlugin({
-					APP_VERSION: JSON.stringify('2.1.0'),
+					APP_VERSION: JSON.stringify('2.1.1'),
 				}),
 				new CleanWebpackPlugin(),
 				new webpack.ProgressPlugin(),
