2eb85bfd8a4aba606fc1c315c13b0a0b719d2226
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-05-03 16:36:35 +0200

    refactor Scoreboard

--- src/js/components/Scoreboard/Layers.js
+++ src/js/components/Scoreboard/Layers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Layer, Group } from 'react-konva'
 
 import { scoreShape } from '../../constants/prop-types'
@@ -19,130 +19,90 @@ import GlobalTouchScreen from './TouchScreen/GlobalTouchScreen'
 import PlayerName from './PlayerName'
 import Z from './Z'
 
-const mapStateToProps = state => ({
-	score: selectScore(state),
-	keyboardOpenFor: selectKeyboardOpenFor(state),
-	playerNames: selectPlayerNames(state),
-})
+const Layers = () =>
+{
+	const dispatch = useDispatch()
 
-const mapDispatchToProps = {
-	addHundred,
-	addFifty,
-	addTwenty,
-	addPoints,
-	openKeyboardForPlayer,
-	setPlayerName,
-	closeKeyboard,
-}
+	const score = useSelector(selectScore)
+	const keyboardOpenFor = useSelector(selectKeyboardOpenFor)
+	const playerNames = useSelector(selectPlayerNames)
 
-const enhance = connect(
-	mapStateToProps,
-	mapDispatchToProps,
-)
+	return (
+		<Layer>
+			<Blackboard key="blackboard"/>
 
-const Layers = ({
-	score,
-	playerNames,
-	keyboardOpenFor,
-	addHundred,
-	addFifty,
-	addTwenty,
-	addPoints,
-	openKeyboardForPlayer,
-	setPlayerName,
-	closeKeyboard,
-}) =>
-	<Layer>
-		<Blackboard key="blackboard"/>
+			<Score key="score-p1" scope={SCOPE_PLAYER_1} score={score[players.PLAYER_1]} />
+			<Score key="score-p2" scope={SCOPE_PLAYER_2} score={score[players.PLAYER_2]} />
 
-		<Score key="score-p1" scope={SCOPE_PLAYER_1} score={score[players.PLAYER_1]} />
-		<Score key="score-p2" scope={SCOPE_PLAYER_2} score={score[players.PLAYER_2]} />
+			<Z key="z-p1" scope={SCOPE_PLAYER_1} />
+			<Z key="z-p2" scope={SCOPE_PLAYER_2} />
 
-		<Z key="z-p1" scope={SCOPE_PLAYER_1} />
-		<Z key="z-p2" scope={SCOPE_PLAYER_2} />
+			<PlayerName
+				key="playername-p1"
+				scope={SCOPE_PLAYER_1}
+				name={playerNames[players.PLAYER_1]}
+				setPlayerName={(scope, name) => dispatch(setPlayerName(scope, name))}
+			/>
+			<PlayerName
+				key="playername-p2"
+				scope={SCOPE_PLAYER_2}
+				name={playerNames[players.PLAYER_2]}
+				setPlayerName={(scope, name) => dispatch(setPlayerName(scope, name))}
+			/>
 
-		<PlayerName
-			key="playername-p1"
-			scope={SCOPE_PLAYER_1}
-			name={playerNames[players.PLAYER_1]}
-			setPlayerName={setPlayerName}
-		/>
-		<PlayerName
-			key="playername-p2"
-			scope={SCOPE_PLAYER_2}
-			name={playerNames[players.PLAYER_2]}
-			setPlayerName={setPlayerName}
-		/>
-
-		{
-			keyboardOpenFor ? null : (
-				<Group>
-					<PlayerTouchScreen
-						key="ts-p1"
+			{
+				keyboardOpenFor ? null : (
+					<Group>
+						<PlayerTouchScreen
+							key="ts-p1"
+							scope={SCOPE_PLAYER_1}
+							onClickHundred={() => dispatch(addHundred(players.PLAYER_1, 1))}
+							onClickFifty={() => dispatch(addFifty(players.PLAYER_1, 1))}
+							onClickTwenty={() => dispatch(addTwenty(players.PLAYER_1, 1))}
+							onClickOne={() => dispatch(openKeyboardForPlayer(players.PLAYER_1))}
+						/>
+						<PlayerTouchScreen
+							key="ts-p2"
+							scope={SCOPE_PLAYER_2}
+							onClickHundred={() => dispatch(addHundred(players.PLAYER_2, 1))}
+							onClickFifty={() => dispatch(addFifty(players.PLAYER_2, 1))}
+							onClickTwenty={() => dispatch(addTwenty(players.PLAYER_2, 1))}
+							onClickOne={() => dispatch(openKeyboardForPlayer(players.PLAYER_2))}
+						/>
+						<GlobalTouchScreen key="ts-g" />
+					</Group>
+				)
+			}
+			{
+				keyboardOpenFor === players.PLAYER_1 ? (
+					<Keyboard
+						key="kb-p1"
 						scope={SCOPE_PLAYER_1}
-						onClickHundred={() => addHundred(players.PLAYER_1, 1)}
-						onClickFifty={() => addFifty(players.PLAYER_1, 1)}
-						onClickTwenty={() => addTwenty(players.PLAYER_1, 1)}
-						onClickOne={() => openKeyboardForPlayer(players.PLAYER_1)}
+						onConfirm={({me, you}) => {
+							const timestamp = Date.now()
+							addPoints(players.PLAYER_1, me, timestamp)
+							you > 0 && dispatch(addPoints(players.PLAYER_2, you, timestamp))
+						}}
+						closeKeyboard={dispatch(closeKeyboard)}
 					/>
-					<PlayerTouchScreen
-						key="ts-p2"
+				) : null
+			}
+			{
+				keyboardOpenFor === players.PLAYER_2 ? (
+					<Keyboard
+						key="kb-p2"
 						scope={SCOPE_PLAYER_2}
-						onClickHundred={() => addHundred(players.PLAYER_2, 1)}
-						onClickFifty={() => addFifty(players.PLAYER_2, 1)}
-						onClickTwenty={() => addTwenty(players.PLAYER_2, 1)}
-						onClickOne={() => openKeyboardForPlayer(players.PLAYER_2)}
+						onConfirm={({ me, you }) => {
+							const timestamp = Date.now()
+							addPoints(players.PLAYER_2, me, timestamp)
+							you > 0 && dispatch(addPoints(players.PLAYER_1, you, timestamp))
+						}}
+						closeKeyboard={dispatch(closeKeyboard)}
 					/>
-					<GlobalTouchScreen key="ts-g" />
-				</Group>
-			)
-		}
-		{
-			keyboardOpenFor === players.PLAYER_1 ? (
-				<Keyboard
-					key="kb-p1"
-					scope={SCOPE_PLAYER_1}
-					onConfirm={({me, you}) => {
-						const timestamp = Date.now()
-						addPoints(players.PLAYER_1, me, timestamp)
-						you > 0 && addPoints(players.PLAYER_2, you, timestamp)
-					}}
-					closeKeyboard={closeKeyboard}
-				/>
-			) : null
-		}
-		{
-			keyboardOpenFor === players.PLAYER_2 ? (
-				<Keyboard
-					key="kb-p2"
-					scope={SCOPE_PLAYER_2}
-					onConfirm={({ me, you }) => {
-						const timestamp = Date.now()
-						addPoints(players.PLAYER_2, me, timestamp)
-						you > 0 && addPoints(players.PLAYER_1, you, timestamp)
-					}}
-					closeKeyboard={closeKeyboard}
-				/>
-			) : null
-		}
-	</Layer>
-
-Layers.propTypes = {
-	score: PropTypes.shape({
-		[players.PLAYER_1]: scoreShape,
-		[players.PLAYER_2]: scoreShape,
-	}),
-	playerNames: PropTypes.shape({
-		[players.PLAYER_1]: PropTypes.string.isRequired,
-		[players.PLAYER_2]: PropTypes.string.isRequired,
-	}),
-	keyboardOpenFor: PropTypes.string,
-	addHundred: PropTypes.func.isRequired,
-	addFifty: PropTypes.func.isRequired,
-	addTwenty: PropTypes.func.isRequired,
-	openKeyboardForPlayer: PropTypes.func.isRequired,
-	setPlayerName: PropTypes.func.isRequired,
-	closeKeyboard: PropTypes.func.isRequired,
+				) : null
+			}
+		</Layer>
+	)
 }
 
-export default enhance(Layers)
+export default Layers
