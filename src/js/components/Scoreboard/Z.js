d8db05e8dadc26a49e9de3e58407802af4ef3095
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-07-25 14:30:02 +0200

    cleanup

--- src/js/components/Scoreboard/Z.js
+++ src/js/components/Scoreboard/Z.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Image, Layer, Line } from 'react-konva'
-import DimensionsConsumer from '../DimensionsContext/DimensionsConsumer'
+import { Line } from 'react-konva'
 import { zColor, strokeWidth } from '../../constants/board'
 import UseDimensionsContext from '../DimensionsContext/UseDimensionsContext'
-import { SCOPE_GLOBAL } from '../DimensionsContext/context'
 
 const Z = ({ scope }) => {
 	const { getPoint } = UseDimensionsContext(scope)
