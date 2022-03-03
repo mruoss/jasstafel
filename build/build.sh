1c63959d50d6abb84720c20b3097977e49cc1118
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-03-03 10:00:56 +0100

    fix build by running yarn install --immutable and using fat build image

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="${DIR}/.."
 
-DOCKER_BUILDKIT=1 docker build -t registry.gitlab.com/mruoss/wedding/jasstafel:latest -f "${DIR}/Dockerfile" "$BASE_DIR"
+DOCKER_BUILDKIT=1 docker build --progress=plain -t registry.gitlab.com/mruoss/wedding/jasstafel:latest -f "${DIR}/Dockerfile" "$BASE_DIR"
